38e3e56608bf837873c30f84efcb7b41e8f3d384
Jack Linton <jacklinton@mac.com>
2017-02-11 15:07:21 -0500

    Computed algorithms. Untested.

--- Gameboard.js
+++ Gameboard.js
@@ -22,11 +22,54 @@ var puzzle [
 	["Thing", "A Package In Your Mail Box"]
 ]
 
-var padA
-var gameA
-var padB
-var gameB
-var padC
-var gameTotal
-var gameWords
-var game
+var rand = function getRandomIntInclusive(0, 21) {
+  min = Math.ceil(0);
+  max = Math.floor(1);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var padA = gameTotal.length/4
+var gameA //#of words in the first line
+var padB = gameTotal.length/2 + gameTotal%2
+var gameB = gameWords.length - gameA //#of words in second line
+var padC = padA
+var gameTotal = game.split("")
+var gameWords = game.split(" ")
+var game = puzzle[rand][1]
+var type = puzzle[rand][0]
+var line1 //words of first line
+var line2 //word of second line
+
+gameA = function(gameWords) {
+	var a = 0
+	var gA = 0
+	function gameAlength(gameWords) {
+		if (gA < 14) {
+			gA += gameWords[a].length + 1
+			a += 1
+			gameAlength(gameWords)
+		} else {
+			return a
+		}
+	}
+	return a
+}
+
+line1 = function(gameA, gameWords) {
+	arry = []
+	for (var i = 0; i < gameA-1; i++) {
+		arry += gameWords[i]
+	}
+	return arry
+}
+
+line2 = function(gameA, gameB, gameWords) {
+	arry = []
+	for (var i = gameA - 1; i < gameB.length; i++) {
+		arry += gameWords[i]
+	}
+	return arry
+}
+
+console.log("Game type: ", type, "Puzzle words:", game, "padA: ", padA, "padB: ", padB, "padC: ", padC, "First Line: ", line1, "Second Line: ", line2)
+
