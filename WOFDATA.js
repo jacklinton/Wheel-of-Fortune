3f2956fa32b36c99039e0e335d24b1fe535f6a59
Jack Linton <jacklinton@mac.com>
2017-02-10 23:22:11 -0500

    Added mush content and calculations in the way of the actual game phrase box. Will use less.js to render the content I think.

---
+++ WOFDATA.js
@@ -0,0 +1,12 @@
+var puzzle [
+	["Thing", "Profitability"],
+	["What Are You Doing?", "Waiting Anxiously"],
+	["Food & Drink", "Flavorful Appetizer"],
+	["Thing", "Vocal Harmony"],
+	["Thing", "Puffy Jacket"],
+	["Phrase", "Busy With Housework"],
+	["Phrase", "Ahead Of The Pack"],
+	["Phase", "What A Crazy Day"],
+	["Thing", "Fancy Handbag"],
+	[]
+]
