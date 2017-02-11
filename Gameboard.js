948bfad7b8091609fb3caba805620d4c210985ba
Jack Linton <jacklinton@mac.com>
2017-02-11 14:27:47 -0500

    Started the construction of the game board algorithm. Defined most of variables.

---
+++ Gameboard.js
@@ -0,0 +1,32 @@
+var puzzle [
+	["Thing", "Profitability"],
+	["What Are You Doing?", "Waiting Anxiously"],
+	["Food & Drink", "Flavorful Appetizer"],
+	["Thing", "Vocal Harmony"],
+	["Thing", "Puffy Jacket"],
+	["Phrase", "Busy With Housework"],
+	["Phrase", "Ahead Of The Pack"],
+	["Phase", "What A Crazy Day"],
+	["Thing", "Fancy Handbag"],
+	["Thing", "Utility Vehicle"],
+	["Phrase", "Downright Histerical"],
+	["Thing", "Popular Opinion"],
+	["What Are You Doing", "Joining A Book Club"],
+	["Things", "Basic Qualifications"],
+	["Phrase","Pop Open The Hood"],
+	["Phrase","On My Way Home"],
+	["Phrase","Hot And Juicy"],
+	["Thing", "Witty Joke"],
+	["Thing", "Privacy Policy"]
+	["Thing", "Gust of Air"]
+	["Thing", "A Package In Your Mail Box"]
+]
+
+var padA
+var gameA
+var padB
+var gameB
+var padC
+var gameTotal
+var gameWords
+var game
